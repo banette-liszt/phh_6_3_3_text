b9788519ead6a791828d4f4abce807227940dfb1
banette-liszt <arisa_16n1100989@nnn.ed.jp>
2017-07-03 15:53:41 +0900

    リスト6-17を反映

--- text.js
+++ text.js
@@ -1,6 +1,21 @@
-document.addEventListener('DOMContentLoaded',function() {
-    document.getElementById('result_text').textContent = 
+document.addEventListener('DOMContentLoaded',function(){
+    document.getElementById('result_text').textContent =
     '<a href="http://www.wings.msn.to/">WINGSプロジェクト</a>';
-    document.getElementById('result_html').innerHTML = 
-    '<a href="http://www.wings.msn.to/">WINGSプロジェクト</a>';　
+    document.getElementById('result_html').innerHTML =
+    '<a href="http://www.wings.msn.to/">WINGSプロジェクト</a>';
+},false);
+
+document.addEventListener('DOMContentLoaded',function(){
+    var list = document.getElementById('list');
+    console.log(list.innerHTML);
+    console.log(list.textContent);
+},false);
+
+document.addEventListener('DOMContentLoaded',function(){
+//ボタンクリック時に挨拶メッセージを反映
+document.getElementById('btn').addEventListener('click',function() {
+    var name = document.getElementById('name');
+    var result = document.getElementById('result');
+    result.innerHTML = 'こんにちは！' + name.value + 'さん＾p＾';
+},false);
 },false);
